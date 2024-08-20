c0027cea61dd2bfc8c75daa5a3d5a30ab01949a4
fuchao <1741108471@qq.com>
2024-08-20 19:52:31 +0800

    add: 添加typecho

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
-
-	echo "hello world";
+$file= __DIR__ . '/..'.$_SERVER["PHP_SELF"];
+if(file_exists($file))
+{
+   return false;
+}
+else
+{
+    require_once __DIR__ . '/../index.php';
+}
+#echo $_SERVER["PHP_SELF"];
